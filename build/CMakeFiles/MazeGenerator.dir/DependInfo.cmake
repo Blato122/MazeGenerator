dd7f38d0362ae2e9ea4d289f9fcd68465eadc432
Blato122 <blator02@gmaii.com>
2023-05-29 17:11:52 +0200

    gitignore build

--- build/CMakeFiles/MazeGenerator.dir/DependInfo.cmake
+++ build/CMakeFiles/MazeGenerator.dir/DependInfo.cmake
@@ -1,19 +1,18 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
   )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/blato/Desktop/ALGORYTMY_2/MazeGenerator/src/DisjointSets.cpp" "/home/blato/Desktop/ALGORYTMY_2/MazeGenerator/build/CMakeFiles/MazeGenerator.dir/src/DisjointSets.cpp.o"
-  "/home/blato/Desktop/ALGORYTMY_2/MazeGenerator/src/Graph.cpp" "/home/blato/Desktop/ALGORYTMY_2/MazeGenerator/build/CMakeFiles/MazeGenerator.dir/src/Graph.cpp.o"
-  "/home/blato/Desktop/ALGORYTMY_2/MazeGenerator/src/Maze.cpp" "/home/blato/Desktop/ALGORYTMY_2/MazeGenerator/build/CMakeFiles/MazeGenerator.dir/src/Maze.cpp.o"
-  "/home/blato/Desktop/ALGORYTMY_2/MazeGenerator/src/Timer.cpp" "/home/blato/Desktop/ALGORYTMY_2/MazeGenerator/build/CMakeFiles/MazeGenerator.dir/src/Timer.cpp.o"
-  "/home/blato/Desktop/ALGORYTMY_2/MazeGenerator/src/main.cpp" "/home/blato/Desktop/ALGORYTMY_2/MazeGenerator/build/CMakeFiles/MazeGenerator.dir/src/main.cpp.o"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
 
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/blato/Desktop/ALGORYTMY_2/MazeGenerator/MazeGenerator/src/DisjointSets.cpp" "CMakeFiles/MazeGenerator.dir/src/DisjointSets.cpp.o" "gcc" "CMakeFiles/MazeGenerator.dir/src/DisjointSets.cpp.o.d"
+  "/home/blato/Desktop/ALGORYTMY_2/MazeGenerator/MazeGenerator/src/Graph.cpp" "CMakeFiles/MazeGenerator.dir/src/Graph.cpp.o" "gcc" "CMakeFiles/MazeGenerator.dir/src/Graph.cpp.o.d"
+  "/home/blato/Desktop/ALGORYTMY_2/MazeGenerator/MazeGenerator/src/Maze.cpp" "CMakeFiles/MazeGenerator.dir/src/Maze.cpp.o" "gcc" "CMakeFiles/MazeGenerator.dir/src/Maze.cpp.o.d"
+  "/home/blato/Desktop/ALGORYTMY_2/MazeGenerator/MazeGenerator/src/Timer.cpp" "CMakeFiles/MazeGenerator.dir/src/Timer.cpp.o" "gcc" "CMakeFiles/MazeGenerator.dir/src/Timer.cpp.o.d"
+  "/home/blato/Desktop/ALGORYTMY_2/MazeGenerator/MazeGenerator/src/main.cpp" "CMakeFiles/MazeGenerator.dir/src/main.cpp.o" "gcc" "CMakeFiles/MazeGenerator.dir/src/main.cpp.o.d"
   )
 
 # Targets to which this target links.
